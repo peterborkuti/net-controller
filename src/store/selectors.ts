77a010e4aee264b118bff8fae7b0ad93d1a7278f
Peter Borkuti <peterborkuti@gmail.com>
2019-01-14 23:00:39 +0100

    state was undefined when component test runs

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -2,12 +2,12 @@ import { createSelector } from '@ngrx/store';
 import { State } from './model';
 import { dictionaryToArray, getDeviceTimeList } from './helpers/helper';
 
-export const selectState = (globalState: any) => globalState.state;
-export const selectChildren = createSelector(selectState, (state: State) => state.children);
-export const selectFlatChildren = createSelector(selectState, (state: State) => dictionaryToArray(state.children));
-export const selectDevices = createSelector(selectState, (state: State) => state.devices);
-export const selectFlatDevices = createSelector(selectState, (state: State) => dictionaryToArray(state.devices));
-export const selectDeviceChild = createSelector(selectState, (state: State) => state.deviceChild);
+export const selectState = (globalState: any) => globalState.state || {};
+export const selectChildren = createSelector(selectState, (state: State) => state.children || {});
+export const selectFlatChildren = createSelector(selectState, (state: State) => dictionaryToArray(state.children || {}));
+export const selectDevices = createSelector(selectState, (state: State) => state.devices || {});
+export const selectFlatDevices = createSelector(selectState, (state: State) => dictionaryToArray(state.devices || {}));
+export const selectDeviceChild = createSelector(selectState, (state: State) => state.deviceChild || {});
 export const selectDeviceTimeDisplay =
   createSelector(selectState,
-    (state: State) => getDeviceTimeList(dictionaryToArray(state.devices), state.deviceTime));
+    (state: State) => getDeviceTimeList(dictionaryToArray(state.devices || {}), state.deviceTime || {}));
