3121f466448ed9eab201e485034ad66522463e39
Peter Borkuti <peterborkuti@gmail.com>
2019-01-16 19:44:47 +0100

    working default device-list spec file

--- src/app/device-list/device-list.component.spec.ts
+++ src/app/device-list/device-list.component.spec.ts
@@ -2,13 +2,45 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeviceListComponent } from './device-list.component';
 
+import { Store, StoreModule } from '@ngrx/store';
+import { State } from '../../store/model';
+import * as MyReducer from '../../store/reducers';
+
+import {
+  MatFormFieldModule, MatListModule, MatInputModule, MatCardModule,
+  MatButtonModule, MatIconModule, MatSelectModule, MatOptionModule,
+  MatTableModule
+} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AddAnonymChild, ModChildName, DeleteChild } from '../../store/actions';
+
+import { FormsModule, ReactiveFormsModule, FormControl, FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
 describe('DeviceListComponent', () => {
   let component: DeviceListComponent;
   let fixture: ComponentFixture<DeviceListComponent>;
+  let reducerSpy: jasmine.Spy;
 
   beforeEach(async(() => {
+    reducerSpy = spyOn(MyReducer, 'reducer').and.callThrough();
     TestBed.configureTestingModule({
-      declarations: [ DeviceListComponent ]
+      declarations: [ DeviceListComponent ],
+      imports: [
+        MatFormFieldModule,
+        MatListModule,
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatTableModule,
+        [StoreModule.forRoot({ state: reducerSpy })],
+        FormsModule, ReactiveFormsModule,
+        BrowserAnimationsModule
+        ]
     })
     .compileComponents();
   }));
@@ -17,6 +49,7 @@ describe('DeviceListComponent', () => {
     fixture = TestBed.createComponent(DeviceListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    reducerSpy.calls.reset();
   });
 
   it('should create', () => {
