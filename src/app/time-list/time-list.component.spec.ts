c4d1cd2f8d38c0810e335e5cef2563211e780637
Peter Borkuti <peterborkuti@gmail.com>
2019-01-21 10:22:57 +0100

    prepare for testing time-list

--- src/app/time-list/time-list.component.spec.ts
+++ src/app/time-list/time-list.component.spec.ts
@@ -2,13 +2,50 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimeListComponent } from './time-list.component';
 
+import { Store, StoreModule } from '@ngrx/store';
+import { State } from '../../store/model';
+import * as MyReducer from '../../store/reducers';
+
+import { TEXT_INPUT_DEBOUNCE_TIME_MS } from '../const';
+
+import {
+  MatFormFieldModule, MatListModule, MatInputModule, MatCardModule,
+  MatButtonModule, MatIconModule, MatSelectModule, MatOptionModule,
+  MatTableModule
+} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AddDevice, ModDevice, DeleteDevice, AddAnonymChild, SetDeviceChild } from '../../store/actions';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { By } from '@angular/platform-browser';
+
 describe('TimeListComponent', () => {
   let component: TimeListComponent;
   let fixture: ComponentFixture<TimeListComponent>;
+  let reducerSpy: jasmine.Spy;
+  let store: Store<State>;
 
   beforeEach(async(() => {
+    reducerSpy = spyOn(MyReducer, 'reducer').and.callThrough();
     TestBed.configureTestingModule({
-      declarations: [ TimeListComponent ]
+      declarations: [ TimeListComponent ],
+      imports: [
+        MatFormFieldModule,
+        MatListModule,
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatTableModule,
+        [StoreModule.forRoot({ state: reducerSpy })],
+        FormsModule, ReactiveFormsModule,
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,7 +53,9 @@ describe('TimeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TimeListComponent);
     component = fixture.componentInstance;
+    store = TestBed.get(Store);
     fixture.detectChanges();
+    reducerSpy.calls.reset();
   });
 
   it('should create', () => {
